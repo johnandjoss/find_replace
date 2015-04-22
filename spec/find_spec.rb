dc4aadaafefc4432ee9ea3db09da5b8953cc7f60
Christopher LeCompte & Jocelyn Alsdorf <student@epicodus.com>
2015-04-22 15:30:26 -0700

    verifies partial matches working

--- spec/find_spec.rb
+++ spec/find_spec.rb
@@ -7,5 +7,8 @@ describe('String#finder') do
   it("find a word and replace it") do
     expect(("Hello world").finder("world", "mom")).to(eq("Hello mom"))
   end
+  it("finds a partial match and replaces it") do
+    expect(("I am walking my cat to the cathedral.").finder("cat", "dog")).to(eq("I am walking my dog to the doghedral."))
+  end
 
 end
